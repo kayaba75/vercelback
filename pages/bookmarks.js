110e1a123e97eafd13272efd29ac94015b318d6a
kayaba75 <fitas.samy@gmail.com>
2023-05-31 16:39:07 +0200

    first commit

---
+++ pages/bookmarks.js
@@ -0,0 +1,6 @@
+import Bookmarks from '../components/Bookmarks';
+
+// L'url de cette page est /bookmarks
+export default function BookmarksPage() {
+  return <Bookmarks />;
+}
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import Home from '@/components/Home';
+
+// L'url de cette page est /
+export default function Index() {
+  return <Home />;
+}
